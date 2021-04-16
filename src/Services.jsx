27dda5d46686f1d2be2c113294a9a2d5719d0c6b
Bapita <bapitaroy14@gmail.com>
2021-04-16 14:02:54 +0530

    all hooks of react-router-dom

--- src/Services.jsx
+++ src/Services.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useParams} from 'react-router-dom';
+import {useParams, useLocation, useHistory} from 'react-router-dom';
 // useParam() is one of the Hooks of react router DOM
 
 // using match, without Hooks
@@ -14,11 +14,33 @@ import {useParams} from 'react-router-dom';
 */
 
 const Services = () => {
-    const { technology, subtech } = useParams();
+    const { technology, subtech } = useParams(); // curly braces shows objects 
+    const location = useLocation(); // to find out the current URL path
+    const history = useHistory(); // to go back and forward 
     return(
         <>
             <h1> {technology} Services page using useParams()</h1>
             <h1> {subtech} Services page using useParams()</h1>
+            <p>My current location is {location.pathname}</p>
+            {location.pathname === `/services/php/java` ? 
+            <>
+                <button style={{margin:20}}
+                onClick={() => {history.push("/")}}>Move to Home Page</button>
+                <button onClick={() => {history.goBack()}}>Move Back</button>
+                <button style={{margin:20}} onClick={() =>{history.goForward()}}>Move Forward</button>
+                <button onClick={() => {alert("Correct URL. Now change it");}}>Alert</button>
+            </>
+                : null
+            }
+            {
+                /*
+                    <button onClick={() => {history.goBack()}}>Move Back</button>
+                    <button onClick={() =>{history.goForward()}}>Move Forward</button>
+                    <button onClick={() => {history.push("/")}}>HomePage</button>
+                    
+                */
+            }
+            
         </>
     );
 }
