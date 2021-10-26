f5acf603437e8313c580d644ac16e507bb5a567d
bapita <bapitaroy14@gmail.com>
2021-10-26 20:05:52 +0530

    updated bootstrapmini proj

--- src/BootstrapMiniProj.jsx
+++ src/BootstrapMiniProj.jsx
@@ -8,32 +8,32 @@ const BootstrapMiniProj = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-sm">
-                        <div className="card">
+                        <div className="card" style={{width:"18rem"}}>
                             <img src="https://picsum.photos/200/200" className="card-img-top" alt="..." height="200px"/>
                                 <div className="card-body">
                                     <h5 className="card-title">Card title</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="http://localhost:3000/" className="btn btn-primary">Go somewhere</a>
+                                    <a href="http://localhost:3000/" className="btn btn-outline-success">Go somewhere</a>
                                 </div>
                         </div>
                     </div>
                     <div className="col-sm">
-                        <div className="card">
+                        <div className="card" style={{width:"18rem"}}>
                             <img src="https://picsum.photos/200/202" className="card-img-top" alt="..." height="200px"/>
                                 <div className="card-body">
                                     <h5 className="card-title">Card title</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="http://localhost:3000/" className="btn btn-primary">Go somewhere</a>
+                                    <a href="http://localhost:3000/" className="btn btn-outline-danger">Go somewhere</a>
                                 </div>
                         </div>
                     </div>
                     <div className="col-sm">
-                        <div className="card">
+                        <div className="card" style={{width:"18rem"}}>
                             <img src="https://picsum.photos/200/203" className="card-img-top" alt="..." height="200px"/>
                                 <div className="card-body">
                                     <h5 className="card-title">Card title</h5>
                                     <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <a href="http://localhost:3000/" className="btn btn-primary">Go somewhere</a>
+                                    <a href="http://localhost:3000/" className="btn btn-outline-warning">Go somewhere</a>
                                 </div>
                         </div>
                     </div>
